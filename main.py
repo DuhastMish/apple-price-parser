fa2a063bdd59665472f9c807b40567adea0c1211
av.maksimov <av.maksimov@tensor.ru>
2024-10-27 13:54:51 +0300

    Moved parser tasks to scheduler

--- main.py
+++ main.py
@@ -3,24 +3,14 @@ Runs parser.
 """
 
 import asyncio
-import time
-from formatter.main import Formatter
-from parser.appzone import AppZoneParser
-from parser.connect import ConnectParser
-from parser.gadgetbar import GadgetBarParser
-from parser.ipoint import iPointParser
-from parser.main import create_base_products_list
-from parser.swype59 import Swype59Parser
-from parser.trade59 import Trade59Parser
 
 from python_utils.logger.main import get_logger
-from sheet.main import TableWorker
 from utils.scheduler import Scheduler
 
 LOGGER = get_logger()
 
 
-async def main():
+if __name__ == "__main__":
     """
     Run parser.
     """
@@ -29,27 +19,8 @@ async def main():
     scheduled_tasks = Scheduler()
     scheduled_tasks.run()
 
-    start_time = time.time()
-
-    product_to_parse = create_base_products_list()
-    Trade59Parser(product_to_parse).get()
-    ConnectParser(product_to_parse).get()
-    iPointParser(product_to_parse).get()
-    Swype59Parser(product_to_parse).get()
-    AppZoneParser(product_to_parse).get()
-    GadgetBarParser(product_to_parse).get()
-
-    product_to_parse.sort(key=lambda product: (product.get("version")))
-
-    Formatter(product_to_parse).format()
-
-    TableWorker().insert(product_to_parse)
-
-    LOGGER.info(f"Paring completed for {round(time.time() - start_time)}")
-
-
-if __name__ == "__main__":
+    # Execution will block here until Ctrl+C (Ctrl+Break on Windows) is pressed.
     try:
-        asyncio.run(main())
+        asyncio.get_event_loop().run_forever()
     except (KeyboardInterrupt, SystemExit):
         LOGGER.error("Parser stopped!")
--- utils/scheduler.py
+++ utils/scheduler.py
@@ -2,16 +2,27 @@
 Асинхронные регулярные задачи.
 """
 
+import time
+from formatter.main import Formatter
+from parser.appzone import AppZoneParser
+from parser.connect import ConnectParser
+from parser.gadgetbar import GadgetBarParser
+from parser.ipoint import iPointParser
+from parser.main import create_base_products_list
+from parser.swype59 import Swype59Parser
+from parser.trade59 import Trade59Parser
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from python_utils.logger.main import get_logger
+from sheet.main import TableWorker
 
 LOGGER = get_logger()
 
 
 class Scheduler:
     """
-    Класс для создания регулярных задач.
+    Class for regular tasks.
     """
 
     def __init__(self):
@@ -21,6 +32,11 @@ class Scheduler:
         """
         Create regular tasks.
         """
+        self.scheduler.add_job(self.regular_parse, "cron", hour=10)
+        self.scheduler.add_job(self.regular_parse, "cron", hour=14)
+        self.scheduler.add_job(self.regular_parse, "cron", hour=20)
+
+        self.scheduler.start()
         LOGGER.info("Regular tasks added")
 
     def stop(self):
@@ -29,3 +45,27 @@ class Scheduler:
         """
         self.scheduler.shutdown()
         LOGGER.info("Regular tasks stopped")
+
+    async def regular_parse(self) -> None:
+        """
+        Parser task.
+        """
+        LOGGER.info("Starting parser")
+
+        start_time = time.time()
+
+        product_to_parse = create_base_products_list()
+        Trade59Parser(product_to_parse).get()
+        ConnectParser(product_to_parse).get()
+        iPointParser(product_to_parse).get()
+        Swype59Parser(product_to_parse).get()
+        AppZoneParser(product_to_parse).get()
+        GadgetBarParser(product_to_parse).get()
+
+        product_to_parse.sort(key=lambda product: (product.get("version")))
+
+        Formatter(product_to_parse).format()
+
+        TableWorker().insert(product_to_parse)
+
+        LOGGER.info(f"Paring completed for {round(time.time() - start_time)}")
